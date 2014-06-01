d7d2e44d0dbb715f0c9f46d99dadf16ccdd45bd3
Caleb Bauermeister <bauermec@onid.oregonstate.edu>
2014-05-31 20:36:57 -0700

    Working velocity system, work on ball-on-ball collisions.

---
+++ 8ball_positions.js
@@ -0,0 +1,15 @@
+[	{ "type" : "position", "position" : { "x" :  2.6, "y" : 0, "z" : 0 }, "ball" : "any" }, //either side of
+	{ "type" : "position", "position" : { "x" : -2.6, "y" : 0, "z" : 0 }, "ball" : "any" }, // the 8 ball
+	{ "type" : "position", "position" : { "x" : 0, "y" : 0, "z" : 0 }, "ball" : "any" },
+	{ "type" : "position", "position" : { "x" : 0, "y" : 0, "z" : 0 }, "ball" : "any" },
+	{ "type" : "position", "position" : { "x" : 0, "y" : 0, "z" : 0 }, "ball" : "any" },
+	{ "type" : "position", "position" : { "x" : 0, "y" : 0, "z" : 0 }, "ball" : "any" },
+	{ "type" : "position", "position" : { "x" : 0, "y" : 0, "z" : 0 }, "ball" : "any" },
+	{ "type" : "position", "position" : { "x" : 0, "y" : 0, "z" : 0 }, "ball" : "any" },
+	{ "type" : "position", "position" : { "x" : 0, "y" : 0, "z" : 0 }, "ball" : "any" },
+	{ "type" : "position", "position" : { "x" : 0, "y" : 0, "z" : 0 }, "ball" : "any" },
+	{ "type" : "position", "position" : { "x" : 0, "y" : 0, "z" : 0 }, "ball" : "any" },
+	{ "type" : "position", "position" : { "x" : 0, "y" : 0, "z" : 0 }, "ball" : "any" },
+	{ "type" : "position", "position" : { "x" : 0, "y" : 0, "z" : 0 }, "ball" : "solid" },
+	{ "type" : "position", "position" : { "x" : 0, "y" : 0, "z" : 0 }, "ball" : "stripe" },
+	{ "type" : "position", "position" : { "x" : 0, "y" : 0, "z" : 0 }, "ball" : "8ball" }]
